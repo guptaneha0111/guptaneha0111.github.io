290943f698902257e77400b3afc56c20c7688507
guptaneha0111 <nehagupta01113@gmail.com>
2023-01-11 01:53:00 +0530

    addedfiles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+// import Link from "react-scroll/modules/components/Link";
+import { Link, animateScroll } from "react-scroll";
+import React from "react";
+import Hamburger from  'hamburger-react';
+import "./Navbar.css";
+
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h1 className="logo-h1" style={{fontFamily:"cursive"}}>Nk</h1>
+      </div>
+      <input id="checkbox" type="checkbox" />
+      <div id="bar">
+        <label htmlFor="checkbox">
+        
+      <Hamburger className="Hamburger"/>
+        </label>
+      </div>
+      <ul className="nav-ul">
+        <Link to="/"
+              activeClass="active"
+              onClick={() => animateScroll.scrollToTop()}
+              spy={true}
+              smooth={true}
+              duration={1000}>
+          <li>Home</li>
+        </Link>
+        <Link  to="about"
+              offset={150}
+
+              smooth={true}
+              duration={1500}
+              spy={true}
+              hashSpy={true}>
+          <li>About</li>
+        </Link>
+        <Link  to="skill"
+              offset={-50}
+              smooth={true}
+              duration={1500}
+              spy={true}
+              hashSpy={true}>
+          <li>Skills</li>
+        </Link>
+        <Link  to="project"
+              offset={-50}
+              smooth={true}
+              duration={1500}
+              spy={true}
+              hashSpy={true}>
+          <li>Projects</li>
+        </Link>
+        <Link to="contact"
+              smooth={true}
+              duration={1000}
+              spy={true}
+              hashSpy={true}>
+          <li>Contact</li>
+        </Link>
+        <a
+          href="https://drive.google.com/file/d/1EgF48O95bq3SSJ2qJ6lNlTQ6wB94AGvS/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+          >
+          <li>Resume</li> 
+          
+        </a>
+      </ul>
+      
+    </div>
+    
+  );
+}
