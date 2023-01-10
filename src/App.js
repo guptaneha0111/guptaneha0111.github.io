4f4887291cb276b846fbdfb527dbe428b4d2d67b
guptaneha0111 <nehagupta01113@gmail.com>
2023-01-11 01:56:49 +0530

    addded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Home/>
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,45 @@
+
+import Typewriter from 'typewriter-effect'
+import React from "react";
+import {BiDownload} from 'react-icons/bi'
+import "./Home.css";
+import  portfoliopersonalimg  from "../../../src/resource/Personal/portperimg.jpg";
+// import { useColorMode } from '@chakra-ui/react';
+
+const Home=()=>{
+  // const { ColorMode, toggleColorMode} = useColorMode();
+  // const isDark = ColorMode === "dark";
+  return (
+
+    <div id="home">
+      <div className="name">
+        <h1 className='text-color f7 hiwalah1'>Hi,<span>👋</span>I am</h1>
+		    <h1 className='text-color f7 nameh123'>Neha Kumari</h1>
+          <h1 className='typewriterh1'>
+              <Typewriter
+                className='auto-type'
+                options={{
+                  strings: [
+                      "A Full Stack Web Developer."
+                     ],
+                   autoStart: true,
+                  loop: true,
+                  deleteSpeed: 80,
+                }}
+              />
+          </h1>
+        <a
+          href="https://drive.google.com/uc?export=download&id=1EgF48O95bq3SSJ2qJ6lNlTQ6wB94AGvS"
+         >
+          <button className='portButton' style={{"border-radius": "10px"}}>Resume<BiDownload className="download"/></button>
+        </a>
+      </div>
+      <div>
+        
+        <img src={portfoliopersonalimg} alt="neha"  className='personalimg' />
+       
+      </div>
+    </div>
+  );
+}
+export default Home;
