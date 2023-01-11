27ea5e087f6f66d36d73cc6fa218a257f231cfa8
guptaneha0111 <nehagupta01113@gmail.com>
2023-01-11 16:28:45 +0530

    addedfiles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Home/>
+    <About/>
     </div>
   );
 }
